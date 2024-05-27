f50f2a268186f58ae3091d7d56d4934e75e319e4
Rahul <you@example.com>
2024-05-27 12:42:42 +0530

    display X when clicking on a square

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,14 @@
-const Square =({value}) => {
+const Square =({value, onClick}) => {
+
+
     
 
-    return<button type="button" className="square">{value}</button>; 
+    return (
+    <button type="button" className="square" onClick={onClick}>
+        {value}
+    </button>
+
+    ); 
 };
 
 export default Square;
